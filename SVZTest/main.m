fd8bdae6815df516a397ea81399006d68e96d968
Lk Xf <elveestei@gmail.com>
2015-11-22 13:16:27 +0100

    Updated test app

--- SVZTest/main.m
+++ SVZTest/main.m
@@ -10,10 +10,50 @@
 #import <SevenZip/SevenZip.h>
 
 int main(int argc, const char * argv[]) {
+    if (argc < 3) {
+        return 1;
+    }
+    
     @autoreleasepool {
-        SVZArchive* archive = [SVZArchive archiveWithURL:[NSURL fileURLWithPath:@"/Users/lvsti/stuff.7z"]
+        NSString* archiveName = [NSString stringWithUTF8String:argv[1]];
+        NSMutableArray* fileArgs = [NSMutableArray arrayWithCapacity:argc-2];
+        for (int i = 2; i < argc; ++i) {
+            [fileArgs addObject:[NSString stringWithUTF8String:argv[i]]];
+        }
+        
+        NSMutableArray* entries = [NSMutableArray array];
+        for (NSString* fileArg in fileArgs) {
+            BOOL isDir = NO;
+            [[NSFileManager defaultManager] fileExistsAtPath:fileArg isDirectory:&isDir];
+            
+            if (isDir) {
+                [entries addObject:[SVZArchiveEntry archiveEntryWithDirectoryName:fileArg.lastPathComponent]];
+                
+                NSDirectoryEnumerator* etor = [[NSFileManager defaultManager] enumeratorAtPath:fileArg];
+                for (NSString* path in etor) {
+                    NSString* fullPath = [fileArg stringByAppendingPathComponent:path];
+                    NSString* fullName = [fileArg.lastPathComponent stringByAppendingPathComponent:path];
+                    NSLog(@"%@", fullName);
+                    [[NSFileManager defaultManager] fileExistsAtPath:fullPath isDirectory:&isDir];
+                    if (isDir) {
+                        [entries addObject:[SVZArchiveEntry archiveEntryWithDirectoryName:fullName]];
+                    }
+                    else {
+                        [entries addObject:[SVZArchiveEntry archiveEntryWithFileName:fullName
+                                                                                 url:[NSURL fileURLWithPath:fullPath]]];
+                    }
+                }
+            }
+            else {
+                NSLog(@"%@", fileArg.lastPathComponent);
+                [entries addObject:[SVZArchiveEntry archiveEntryWithFileName:fileArg.lastPathComponent
+                                                                         url:[NSURL fileURLWithPath:fileArg]]];
+            }
+        }
+
+        SVZArchive* archive = [SVZArchive archiveWithURL:[NSURL fileURLWithPath:archiveName]
                                                    error:NULL];
-        [archive addFileAtURL:[NSURL fileURLWithPath:@"/Users/lvsti/respoof"]];
+        [archive updateEntries:entries error:NULL];
     }
     return 0;
 }
