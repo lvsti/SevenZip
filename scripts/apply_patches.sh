5df419ca26fc37e01dcfaabc140b6d254cc89c41
Lk Xf <elveestei@gmail.com>
2015-11-22 23:45:49 +0100

    Added setup script

---
+++ scripts/apply_patches.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -f External/p7zip/CPP/include_windows/windows.h ]; then
+    echo "error: windows.h not found, make sure you have the p7zip source in place."
+    exit 1
+fi
+
+patch -p1 < scripts/windows_h_patch.diff
+
+if [ ! -f External/p7zip/C/7zCrc.c ]; then
+    echo "error: 7zCrc.c not found, make sure you have the p7zip source in place."
+    exit 1
+fi
+
+patch -p1 < scripts/7zCrc_c_patch.diff
---
+++ scripts/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p External
+
+echo "Downloading p7zip..."
+TEMP_BZ2=$(mktemp -t p7zip)
+curl -L -o "${TEMP_BZ2}" http://sourceforge.net/projects/p7zip/files/latest/download
+
+echo "Unpacking..."
+TEMP_TAR=$(mktemp -t p7zip)
+bunzip2 -c "${TEMP_BZ2}" > "${TEMP_TAR}"
+
+tar -xf "${TEMP_TAR}" -C External
+
+rm -rf External/p7zip
+ln -s $(ls External | grep p7zip) External/p7zip
+
+rm -f "${TEMP_TAR}" "${TEMP_BZ2}"
+
+echo "Applying patches..."
+scripts/apply_patches.sh
+
+echo "Done!"
