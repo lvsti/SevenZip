eeb25cd914d63c7bdc69b33daf51b8d54ab9295d
Lk Xf <elveestei@gmail.com>
2015-12-01 21:42:54 +0100

    Handled possible stream failure + remembered to have to open the stream...

--- SevenZip/SVZForwardingStream.mm
+++ SevenZip/SVZForwardingStream.mm
@@ -14,17 +14,32 @@ namespace SVZ {
     STDMETHODIMP ForwardingStream::Read(void* data, UInt32 size, UInt32* processedSize) {
         @autoreleasepool {
             NSInteger bytesRead = [_source read:(uint8_t*)data maxLength:size];
+            if (bytesRead < 0) {
+                // stream error
+                if (processedSize) {
+                    *processedSize = 0;
+                }
+                return E_FAIL;
+            }
+            
             if (processedSize) {
                 *processedSize = (UInt32)bytesRead;
             }
         }
+
         return S_OK;
     }
     
     ForwardingStream::ForwardingStream(NSInputStream* source): _source(source) {
+        @autoreleasepool {
+            [_source open];
+        }
     }
     
     ForwardingStream::~ForwardingStream() {
+        @autoreleasepool {
+            [_source close];
+        }
         _source = nil;
     }
 }
