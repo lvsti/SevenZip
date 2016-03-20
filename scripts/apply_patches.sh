9c00f68a003288745b82b492528c8b5f079afa29
Tamas Lustyik <elveestei@gmail.com>
2016-03-20 20:30:56 +0100

    Added patch to make the sources compile

--- scripts/apply_patches.sh
+++ scripts/apply_patches.sh
@@ -22,3 +22,10 @@ if [ ! -f External/p7zip/C/7zTypes.h ]; then
 fi
 
 patch -p1 < scripts/7zTypes_h_patch.diff
+
+if [ ! -f External/p7zip/CPP/Windows/FileDir.h ]; then
+    echo "error: FileDir.h not found, make sure you have the p7zip source in place."
+    exit 1
+fi
+
+patch -p1 < scripts/FileDir_h_patch.diff
