ca26320f60d5c9cb20111dfb46dfccfbdac753c1
Andras Koczka <ndcube@gmail.com>
2019-02-26 13:48:22 +0100

    Use new url for p7zip, the old one is no longer available

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ mkdir -p External
 
 echo "Downloading p7zip..."
 TEMP_BZ2=$(mktemp -t p7zip)
-curl -L -o "${TEMP_BZ2}" https://netix.dl.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2
+curl -L -o "${TEMP_BZ2}" https://kent.dl.sourceforge.net/project/p7zip/p7zip/16.02/p7zip_16.02_src_all.tar.bz2
 
 echo "Unpacking..."
 TEMP_TAR=$(mktemp -t p7zip)
